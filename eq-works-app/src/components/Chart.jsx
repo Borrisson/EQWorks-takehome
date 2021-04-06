9cb272a16d9ef652f1aeda815a772e5640ab6367
Luc <lucborris@gmail.com>
2021-04-06 11:56:57 -0400

    Add chart to chart component

--- eq-works-app/src/components/Chart.jsx
+++ eq-works-app/src/components/Chart.jsx
@@ -1,3 +1,36 @@
-export default function Chart(props) {
-  return <canvas id="myChart" width="400" height="400"></canvas>;
+import Chart from "chart.js/auto";
+
+import { useEffect } from "react";
+
+export default function Graph(props) {
+  useEffect(() => {
+    const ctx = document.getElementById("myChart");
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "Red",
+              "Blue",
+              "Yellow",
+              "Green",
+              "Purple",
+              "Orange",
+            ],
+            borderColor: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            borderWidth: 1,
+          },
+        ],
+      },
+    });
+  }, []);
+  return (
+    <div id="chartContainer">
+      <canvas id="myChart" />
+    </div>
+  );
 }
