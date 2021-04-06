e477a123d3ef98e983f978b9199a4230021f806a
Luc <lucborris@gmail.com>
2021-04-06 09:09:55 -0400

    Add time till reset message for rate limiter

--- eq-works-api/app.rb
+++ eq-works-api/app.rb
@@ -2,6 +2,12 @@ require "dotenv/load"
 require "sinatra"
 require "sinatra/json"
 require "pg"
+require "redis"
+REDIS = Redis.new
+
+before do
+  throttle
+end
 
 get "/" do
   "Welcome to EQ Works 😎"
@@ -53,6 +59,26 @@ get "/poi" do
     SELECT *
     FROM public.poi;
   " ""
+end
+
+private
+
+def throttle
+  client_ip = request.env["REMOTE_ADDR"]
+  key = "count:#{client_ip}"
+  count = REDIS.get(key)
+
+  unless count
+    REDIS.set(key, 0)
+    REDIS.expire(key, ENV["THROTTLE_TIME_WINDOW"].to_i)
+    return true
+  end
+
+  if count.to_i >= ENV["THROTTLE_MAX_REQUESTS"].to_i
+    halt 429, { "Content-Type" => "text/plain" }, "You have reached you're maximum api requests of #{ENV["THROTTLE_MAX_REQUESTS"]}. You will need to wait until #{Time.at(Time.now.to_i + REDIS.ttl(key))} in order for it to reset"
+  end
+  REDIS.incr(key)
+  true
 end
 
 def query_helper(query)
