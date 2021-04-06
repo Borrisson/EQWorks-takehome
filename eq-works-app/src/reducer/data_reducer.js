59296955af6ecd3568b8b417131982873f799aa0
Luc <lucborris@gmail.com>
2021-04-06 10:23:44 -0400

    Add data_reducer action logic

--- eq-works-app/src/reducer/data_reducer.js
+++ eq-works-app/src/reducer/data_reducer.js
@@ -1,9 +1,17 @@
 export const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const dataReducer = (state, { type }) => {
+const dataReducer = (
+  state,
+  { type, hourlyEvents, dailyEvents, hourlyStats, dailyStats, poi }
+) => {
   switch (type) {
     case SET_APPLICATION_DATA:
       return {
         ...state,
+        hourlyEvents,
+        dailyEvents,
+        hourlyStats,
+        dailyStats,
+        poi,
       };
     default:
       throw new Error(`Tried to reduce with unsupported action type: ${type}`);
