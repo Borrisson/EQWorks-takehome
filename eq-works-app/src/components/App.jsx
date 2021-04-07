c818243aa42549ea7d5d24ff9aa7f4c4e6086bcd
Luc <lucborris@gmail.com>
2021-04-07 11:53:27 -0400

    Add views manager state handler

--- eq-works-app/src/components/App.jsx
+++ eq-works-app/src/components/App.jsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./styles/App.scss";
 import useApplicationData from "../hooks/useAppData";
 import SideBar from "./Sidebar";
 import Chart from "./Chart";
+import Dashboard from "./Dashboard";
+import Map from "./Map";
+
+const CHART = "CHART";
+const DASHBOARD = "DASHBOARD";
+const MAP = "MAP";
 
 function App() {
   const { state } = useApplicationData();
+  const [view, setView] = useState(CHART);
   return (
     // add a loader until data has been fetched
     <>
       <SideBar />
-      <Chart state={state} />
+      {view === CHART && <Chart state={state} />}
+      {view === DASHBOARD && <Dashboard />}
+      {view === MAP && <Map />}
     </>
   );
 }
