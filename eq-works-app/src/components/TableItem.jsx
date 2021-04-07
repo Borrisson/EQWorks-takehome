f4bc46bf2ed5b9754fc135d7e61f03aa0554b46a
Luc <lucborris@gmail.com>
2021-04-07 12:31:15 -0400

    Remove date constructor from TableItem

--- eq-works-app/src/components/TableItem.jsx
+++ eq-works-app/src/components/TableItem.jsx
@@ -1,11 +1,10 @@
 import format from "date-fns/format";
 
 export default function TableItem({ id, date, events }) {
-  const dateObj = new Date(date);
   return (
     <tr>
       <td>{id}</td>
-      <td>{format(dateObj, "MM/dd/yyyy")}</td>
+      <td>{format(date, "MM/dd/yyyy")}</td>
       <td>{format(date, "hh aaaa")}</td>
       <td>{events}</td>
     </tr>
