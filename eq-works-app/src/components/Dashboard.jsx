b1904eaea17e6dbed2b79dee32a9a921de676d77
Luc <lucborris@gmail.com>
2021-04-07 12:13:51 -0400

    Passed state to Dashboard

--- eq-works-app/src/components/Dashboard.jsx
+++ eq-works-app/src/components/Dashboard.jsx
@@ -1,35 +1,18 @@
 import Table from "react-bootstrap/Table";
+import TableItem from "./TableItem";
 
-export default function Dashboard(props) {
+export default function Dashboard(state) {
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
+          <th>Date</th>
+          <th>Hour</th>
+          <th>Events</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
+      <tbody></tbody>
     </Table>
   );
 }
---
+++ eq-works-app/src/components/TableItem.jsx
@@ -0,0 +1,10 @@
+export default function TableItem(props) {
+  return (
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  );
+}
