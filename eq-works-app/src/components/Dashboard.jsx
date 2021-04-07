a485c19e818fae702d14dae3639b0263dd118f56
Luc <lucborris@gmail.com>
2021-04-07 13:37:28 -0400

    Add Pagination items to Dashboard

--- eq-works-app/src/components/Dashboard.jsx
+++ eq-works-app/src/components/Dashboard.jsx
@@ -1,14 +1,31 @@
 import Table from "react-bootstrap/Table";
 import Pagination from "react-bootstrap/Pagination";
 import TableItem from "./TableItem";
+import { useState } from "react";
+const HOURLYEVENTS = "hourlyEvents";
+const HOURLYSTATS = "hourlyStats";
+const POI = "poi";
 
 export default function Dashboard(state) {
-  const parsedTableItem = state.hourlyEvents.map((el, id) => (
+  const [view, setView] = useState(HOURLYEVENTS);
+
+  const parsedTableItem = state[view].map((el, id) => (
     <TableItem key={el.date} id={id} {...el} />
   ));
+
   return (
     <div className="dashboard">
-      <Pagination>{1}</Pagination>
+      <Pagination>
+        <Pagination.Item key={HOURLYEVENTS} active={view === HOURLYEVENTS}>
+          Events
+        </Pagination.Item>
+        <Pagination.Item key={HOURLYSTATS} active={view === HOURLYSTATS}>
+          Stats
+        </Pagination.Item>
+        <Pagination.Item key={POI} active={view === POI}>
+          Points of Interest
+        </Pagination.Item>
+      </Pagination>
 
       <Table striped bordered hover>
         <thead>
