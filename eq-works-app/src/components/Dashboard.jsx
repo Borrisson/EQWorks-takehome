dfaa54d29d46d337dbb1b95bd8288780fd38c2a9
Luc <lucborris@gmail.com>
2021-04-07 17:08:00 -0400

    Remove button import

--- eq-works-app/src/components/Dashboard.jsx
+++ eq-works-app/src/components/Dashboard.jsx
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Pagination from "react-bootstrap/Pagination";
 import Form from "react-bootstrap/Form";
 import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
 import TableItem from "./TableItem";
 import { useState } from "react";
 const HOURLYEVENTS = "hourlyEvents";
