ef3c311e73843abeb3bcf4433bc34201046844ab
Luc <lucborris@gmail.com>
2021-04-07 12:30:16 -0400

    Add table parsed with date

--- eq-works-app/src/components/Dashboard.jsx
+++ eq-works-app/src/components/Dashboard.jsx
@@ -2,16 +2,20 @@ import Table from "react-bootstrap/Table";
 import TableItem from "./TableItem";
 
 export default function Dashboard(state) {
+  const parsedTableItem = state.hourlyEvents.map((el, id) => (
+    <TableItem key={el.date} id={id} {...el} />
+  ));
   return (
     <Table striped bordered hover>
       <thead>
         <tr>
           <th>#</th>
-          <th>DateTime</th>
+          <th>Date</th>
+          <th>Time</th>
           <th>Events</th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>{parsedTableItem}</tbody>
     </Table>
   );
 }
--- eq-works-app/src/components/TableItem.jsx
+++ eq-works-app/src/components/TableItem.jsx
@@ -1,8 +1,12 @@
+import format from "date-fns/format";
+
 export default function TableItem({ id, date, events }) {
+  const dateObj = new Date(date);
   return (
     <tr>
       <td>{id}</td>
-      <td>{date}</td>
+      <td>{format(dateObj, "MM/dd/yyyy")}</td>
+      <td>{format(date, "hh aaaa")}</td>
       <td>{events}</td>
     </tr>
   );
