d13fc77276b2ad6af2a492f094d354937293845b
Luc <lucborris@gmail.com>
2021-04-09 10:08:16 -0400

    rollback prior to markerclustergroup

--- eq-works-app/src/components/Map/Map.jsx
+++ eq-works-app/src/components/Map/Map.jsx
@@ -1,6 +1,5 @@
 import { MapContainer, TileLayer, MapConsumer } from "react-leaflet";
 import MarkerItem from "./MarkerItem";
-import MarkerClusterGroup from "react-leaflet-markercluster";
 
 export default function Map({ poi }) {
   const parsedMarkers = poi.map((obj) => {
@@ -13,13 +12,13 @@ export default function Map({ poi }) {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <MarkerClusterGroup>{parsedMarkers}</MarkerClusterGroup>
       <MapConsumer>
         {(map) => {
           console.log("map center:", map.getCenter());
           return null;
         }}
       </MapConsumer>
+      {parsedMarkers}
     </MapContainer>
   );
 }
